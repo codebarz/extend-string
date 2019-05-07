5477b00e3fd53a10d903871d2ca380304068b908
Tega Oke <oketegah@gmail.com>
2019-05-07 17:22:09 +0100

    Added method to convert to lowercase

--- extend-string.js
+++ extend-string.js
@@ -2,10 +2,28 @@ String.prototype.hasVowels = function () {
   return (/[aeiou]/g).test(this);
 };
 String.prototype.toUpper = function () {
-
+  let stringArray = this.split("");
+  let regExPatter = /^[a-z][a-z]*/g;
+  let capitalizedString = [];
+  if(regExPatter.test(this)) {
+    for(let index = 0; index < stringArray.length; index++) {
+      capitalizedString.push(String.fromCharCode(stringArray[index].charCodeAt(stringArray[index]) - 32));
+    }
+    return capitalizedString.join("");
+  }
+  return "String is already uppercase";
 };
 String.prototype.toLower = function () {
-
+  let stringArray = this.split("");
+  let regExPatter = /^[A-Z][a-zA-Z]*/g;
+  let capitalizedString = [];
+  if(regExPatter.test(this)) {
+    for(let index = 0; index < stringArray.length; index++) {
+      capitalizedString.push(String.fromCharCode(stringArray[index].charCodeAt(stringArray[index]) + 32));
+    }
+    return capitalizedString.join("");
+  }
+  return "String is already lowercase";
 };
 String.prototype.ucFirst = function () {
 
--- test/extend-string.test.js
+++ test/extend-string.test.js
@@ -11,7 +11,7 @@ describe("Extended String Methods", function () {
     expect(testString.toUpper()).toBe("MICHAEL")
   });
   it("Should check if all string characters can be converted to lower case", function () {
-    expect(testString.toLower()).toBe("michael");
+    expect(testString.toUpperCase().toLower()).toBe("michael");
   });
   it("Should check conversion of first character to upper case", function () {
     expect(testString.ucFirst()).toBe("Michael");
