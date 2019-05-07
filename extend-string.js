17c7128909ee8e654bc20f84a77a7d82a351082f
Tega Oke <oketegah@gmail.com>
2019-05-07 15:11:46 +0100

    Added method to check if it a question and get out words

--- extend-string.js
+++ extend-string.js
@@ -1,5 +1,20 @@
 String.prototype.hasVowels = function () {
   return (/[aeiou]/g).test(this);
 };
+String.prototype.toUpper = function () {
+
+};
+String.prototype.toLower = function () {
+
+};
+String.prototype.ucFirst = function () {
+
+};
+String.prototype.isQuestion = function () {
+  return (/\?$/g).test(this);
+};
+String.prototype.words = function () {
+  return this.match(/[a-zA-z]+/g);
+};
 
 module.exports = String.prototype;
