9be2f8eb0e8831c799e17e10aba98c9f96f1a90f
Tega Oke <oketegah@gmail.com>
2019-05-07 13:51:22 +0100

    Added test case for converting to upper

--- test/extend-string.test.js
+++ test/extend-string.test.js
@@ -1,8 +1,10 @@
 let string = require("../extend-string");
-
+let testString = "Michael";
 describe("Extended String Methods", function() {
   it("Should return boolean if string contains a vowel", function() {
-    let testString = "Michael";
     expect(testString.hasVowels()).toBeTruthy();
+  });
+  it("Should check if all string characters can be converted to upper case", function() {
+    expect(testString.toUpper()).toBe("MICHAEL")
   });
 });
