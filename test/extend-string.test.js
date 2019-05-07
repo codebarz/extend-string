f34eca5a9f7d6aa9916bf7b51d399196074be2bf
Tega Oke <oketegah@gmail.com>
2019-05-07 14:06:07 +0100

    added test case to convert currency to digits

--- test/extend-string.test.js
+++ test/extend-string.test.js
@@ -1,6 +1,7 @@
 let string = require("../extend-string");
 let testString = "Michael";
 let sentence = "What is my name?";
+let currencyDigits = "11111.11";
 describe("Extended String Methods", function () {
   it("Should return boolean if string contains a vowel", function () {
     expect(testString.hasVowels()).toBeTruthy();
@@ -20,5 +21,14 @@ describe("Extended String Methods", function () {
   });
   it("Should return a list of words in an array from string", function () {
     expect(sentence.words()).toEqual(["What", "is", "my", "name"]);
+  });
+  it("Should check the amout of words in a sentence", function () {
+    expect(sentence.wordCount()).toEqual(4);
+  });
+  it("Should return currency representation", function () {
+    expect(currencyDigits.toCurrency()).toBe("11,111.11");
+  });
+  it("Should return number representation of currency", function () {
+    expect(currency.fromCurrency()).toBe("11111.11");
   });
 });
