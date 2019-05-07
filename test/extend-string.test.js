8a3c40e6db0d15d0db76668bf267ff2a4dc1336e
Tega Oke <oketegah@gmail.com>
2019-05-07 14:12:59 +0100

    Added testcase to convert number to words

--- test/extend-string.test.js
+++ test/extend-string.test.js
@@ -1,7 +1,8 @@
 let string = require("../extend-string");
-let testString = "Michael";
+let testString = "michael";
 let sentence = "What is my name?";
 let currencyDigits = "11111.11";
+let aNumber = "325";
 describe("Extended String Methods", function () {
   it("Should return boolean if string contains a vowel", function () {
     expect(testString.hasVowels()).toBeTruthy();
@@ -33,5 +34,11 @@ describe("Extended String Methods", function () {
   });
   it("Should return each letter in a string as inverse", function () {
     expect(sentence.inverseCase()).toBe("wHAT is my name");
+  });
+  it("Should check if string can be alternated", function () {
+    expect(testString.alternatingCase()).toBe("mIcHaEl");
+  });
+  it("Should check if number can be converted to words", function () {
+    expect(aNumber.numberWords()).toBe("three two five");
   });
 });
