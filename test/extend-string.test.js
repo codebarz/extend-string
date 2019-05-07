9ca7f9d6e795f009fdcb6368eedb94fd041a8bf4
Tega Oke <oketegah@gmail.com>
2019-05-07 14:00:12 +0100

    Added test case to check for words

--- test/extend-string.test.js
+++ test/extend-string.test.js
@@ -1,19 +1,24 @@
 let string = require("../extend-string");
 let testString = "Michael";
-describe("Extended String Methods", function() {
-  it("Should return boolean if string contains a vowel", function() {
+let sentence = "What is my name?";
+describe("Extended String Methods", function () {
+  it("Should return boolean if string contains a vowel", function () {
     expect(testString.hasVowels()).toBeTruthy();
   });
-  it("Should check if all string characters can be converted to upper case", function() {
+  it("Should check if all string characters can be converted to upper case", function () {
     expect(testString.toUpper()).toBe("MICHAEL")
   });
-  it("Should check if all string characters can be converted to lower case", function() {
+  it("Should check if all string characters can be converted to lower case", function () {
     expect(testString.toLower()).toBe("michael");
   });
-  it("Should check conversion of first character to upper case", function() {
+  it("Should check conversion of first character to upper case", function () {
     expect(testString.ucFirst()).toBe("Michael");
   });
   it("Should check if a string is a question", function () {
     expect(testString.isQuestion()).toBeFalsy();
-  })
+    expect(sentence.isQuestion()).toBeTruthy();
+  });
+  it("Should return a list of words in an array from string", function () {
+    expect(sentence.words()).toEqual(["What", "is", "my", "name"]);
+  });
 });
